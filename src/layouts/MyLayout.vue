adb6e4839624706f87060faab0f31414bcc06333
Max <zhenghao.zhu@daocloud.io>
2019-03-06 15:01:05 +0800

    init project

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="hhh lpR fFf">
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar-framework.org/img/quasar-logo.png" />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="right" side="right" elevated>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      right: true,
+    };
+  },
+};
+</script>
